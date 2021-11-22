d5da7e5245f2393ff14268ee46e7fbf949b4c998
Chistaykov Aleksey <chskela@gmail.com>
2021-11-22 21:02:26 +0300

    Done StartActivity design

--- app/src/main/java/com/chskela/weatherappandroid/StartActivity.kt
+++ app/src/main/java/com/chskela/weatherappandroid/StartActivity.kt
@@ -1,13 +1,24 @@
 package com.chskela.weatherappandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+import android.widget.Button
+import kotlinx.coroutines.delay
 
 class StartActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val btn = findViewById<Button>(R.id.button)
+
+        btn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
